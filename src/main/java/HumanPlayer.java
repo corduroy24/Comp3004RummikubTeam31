d84781eef16e66a1d6d6ad3514608bbb46eb89f1
korede97 <korede97@users.noreply.github.com>
2018-10-04 18:13:53 -0400

    Added comment

--- src/main/java/HumanPlayer.java
+++ src/main/java/HumanPlayer.java
@@ -7,23 +7,31 @@ public class HumanPlayer {
 
 	
 	public HumanPlayer () {
-		isTurn = true;
+		isTurn = true; //Human player by default has the first turn in our game
 	}
 	
-	public boolean getIsTurn () {
+	public boolean getIsTurn () { //gets the current status of the players turn  
 		return this.isTurn; 
 	}
 	
-	public void setIsTurn(boolean turn) {
+	public void setIsTurn(boolean turn) { //sets the turn of the player to true or false 
 		this.isTurn  = turn; 
 	}
 	
-	public boolean getIsTileTaken () {
-		return this.isTurn; 
+	public boolean getIsTileTaken () { //gets the status of a player taking a tile 
+		return this.isTileTaken; 
 	}
 	
-	public void setIsTileTaken(boolean turn) {
-		this.isTurn  = turn; 
+	public void setIsTileTaken(boolean tileTaken) { //sets the status of the player taking a tile or not 
+		this.isTileTaken  = tileTaken; 
+	}
+	
+	public boolean getIsTilePlaced () { //gets the status of a player placing a tile
+		return this.isTilePlaced; 
+	}
+	
+	public void setIsTilePlaced(boolean tilePlaced) {//sets the status of the placing a tile or not 
+		this.isTilePlaced  = tilePlaced; 
 	}
 	
 }
--- src/test/java/HumanTest.java
+++ src/test/java/HumanTest.java
@@ -2,7 +2,7 @@ import junit.framework.TestCase;
 
 public class HumanTest extends TestCase{
 	
-	public void  testTurn() {
+	public void  testTurn() { //tests if its the players turn or not 
 		
 		HumanPlayer humanPlayer = new HumanPlayer ();
 		assertTrue(humanPlayer.getIsTurn()); 
@@ -11,6 +11,14 @@ public class HumanTest extends TestCase{
 	}
 	
 	public void testTileTaken() {
+		
+		HumanPlayer humanPlayer = new HumanPlayer ();
+		assertTrue(humanPlayer.getIsTileTaken()); 
+		humanPlayer.setIsTileTaken(false); 
+		assertFalse(humanPlayer.getIsTileTaken()); 
+	}
+	
+	public void testTilePlaced() {
 		
 		HumanPlayer humanPlayer = new HumanPlayer ();
 		assertTrue(humanPlayer.getIsTileTaken()); 
