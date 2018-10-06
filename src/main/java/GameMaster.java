913d74fbed699a92be7794cf57fff6bd52aa557a
korede97 <korede97@users.noreply.github.com>
2018-10-06 18:17:36 -0400

    refactored code

--- src/main/java/GameMaster.java
+++ src/main/java/GameMaster.java
@@ -5,7 +5,13 @@ enum GameState {Game_Start, End_Turn, End_Game};
 public class GameMaster extends Observable{
     
 	private GameState state;  
+	int currTurn = 0; 
 	
+	public GameMaster () {
+		this.currTurn = 1;
+		this.state = state.Game_Start;
+		//setGameState(0)
+	}
 	public GameState getGameState() {
 		return this.state;
 	}
@@ -17,7 +23,10 @@ public class GameMaster extends Observable{
 	}
 	
 	public void endTurn() {
-		
+		if(currTurn  == 4)
+			this.currTurn  = 1; 
+		else 
+			currTurn++; 
 	}
 
 }
