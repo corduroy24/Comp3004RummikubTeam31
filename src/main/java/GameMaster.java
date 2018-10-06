71d2cc2373f9d0990123be6c30233c12e01a243f
korede97 <korede97@users.noreply.github.com>
2018-10-06 16:28:33 -0400

    Started implmeneting Observer Pattern 	- refactoring code 	- added game states, as events. So different functions can be used based on those states.

--- src/main/java/GameMaster.java
+++ src/main/java/GameMaster.java
@@ -1,5 +1,23 @@
 import java.util.Observable;
 
+enum GameState {Game_Start, End_Turn, End_Game};
+
 public class GameMaster extends Observable{
+    
+	private GameState state;  
+	
+	public GameState getGameState() {
+		return this.state;
+	}
+	
+	public void setGameState(GameState currState) {		
+		this.state = currState;
+		setChanged(); 
+		notifyObservers(); 
+	}
+	
+	public void endTurn() {
+		
+	}
 
 }
