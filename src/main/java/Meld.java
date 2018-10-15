61d519430ee3f776a5f6d842bb5d51ce118df32c
korede97 <korede97@users.noreply.github.com>
2018-10-15 08:17:40 -0400

    created and failed test for checking the initial status of a meld

---
+++ src/main/java/Meld.java
@@ -0,0 +1,15 @@
+
+public class Meld {
+	
+	private boolean status; 
+	
+	public Meld () {
+		
+	}
+	
+	public boolean getStatus() {
+		
+		return false; 
+	}
+
+}
---
+++ src/test/java/MeldTest.java
@@ -0,0 +1,22 @@
+import junit.framework.TestCase;
+
+public class MeldTest extends TestCase {
+
+	public void testInitialMeldStatus() {
+		
+		PlayerHand hand = new PlayerHand ("x"); 
+		Tile t1 = new Tile(1,10); 
+		Tile t2 = new Tile(1, 11); 
+		Tile t3 = new Tile(1,12); 
+		Tile t4 = new Tile(1,13); 
+		
+		hand.addTileToHand(t1);
+		hand.addTileToHand(t2);
+		hand.addTileToHand(t3);
+		hand.addTileToHand(t4);
+
+		Meld meld = new Meld (hand);
+		
+		assertTrue(meld.validate());
+	}
+}
