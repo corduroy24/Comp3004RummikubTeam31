540b60ee42bd87abc108bba5a9663d0c9c9c0005
korede97 <korede97@users.noreply.github.com>
2018-10-15 09:26:19 -0400

    tested every if statement inside checkRun and failed

--- src/test/java/MeldTest.java
+++ src/test/java/MeldTest.java
@@ -5,15 +5,11 @@ public class MeldTest extends TestCase {
 	public void testInitialMeld() {
 		
 		PlayerHand hand = new PlayerHand ("x"); 
-		Tile t1 = new Tile(1,10); 
-		Tile t2 = new Tile(1,11); 
-		Tile t3 = new Tile(1,12); 
-		Tile t4 = new Tile(1,13); 
+		hand.addTileToHand(new Tile(1,9));
+		hand.addTileToHand(new Tile (1,10));
+		hand.addTileToHand(new Tile (1,11));
+		hand.addTileToHand(new Tile (1,12));
 		
-		hand.addTileToHand(t1);
-		hand.addTileToHand(t2);
-		hand.addTileToHand(t3);
-		hand.addTileToHand(t4);
 
 		Meld meld = new Meld (hand.getTiles());
 		
@@ -22,21 +18,42 @@ public class MeldTest extends TestCase {
 	
 	public void testCheckRun() {
 		
-		PlayerHand hand = new PlayerHand ("x"); 
-		Tile t1 = new Tile(1,10); 
-		Tile t2 = new Tile(1,11); 
-		Tile t3 = new Tile(1,12); 
-		Tile t4 = new Tile(1,13); 
+		//// valid run 
+		PlayerHand h1 = new PlayerHand ("x"); 
 		
-		hand.addTileToHand(t1);
-		hand.addTileToHand(t2);
-		hand.addTileToHand(t3);
-		hand.addTileToHand(t4);
+		h1.addTileToHand(new Tile(1,9));
+		h1.addTileToHand(new Tile (1,10));
+		h1.addTileToHand(new Tile (1,11));
+		h1.addTileToHand(new Tile (1,12));
 
-		Meld meld = new Meld (hand.getTiles());
+		Meld meld1 = new Meld (h1.getTiles());
 		
-		assertTrue(meld.checkRun(meld.getTiles())); 
+		assertTrue(meld1.checkRun(meld1.getTiles()));
 		
+		///// in valid run with different colour 
+		PlayerHand h2 = new PlayerHand ("x"); 
 		
+		h2.addTileToHand(new Tile(1,9));
+		h2.addTileToHand(new Tile (1,10));
+		h2.addTileToHand(new Tile (1,11));
+		h2.addTileToHand(new Tile (1,12));
+
+		Meld meld2 = new Meld (h2.getTiles());
+		
+		meld2.addTile(new Tile(2,13));
+		assertTrue(meld2.checkRun(meld2.getTiles()));
+		
+		//////invalid run with different number 
+		PlayerHand h3 = new PlayerHand ("x"); 
+		
+		h3.addTileToHand(new Tile(1,9));
+		h3.addTileToHand(new Tile (1,10));
+		h3.addTileToHand(new Tile (1,11));
+		h3.addTileToHand(new Tile (1,12));
+
+		Meld meld3 = new Meld (h2.getTiles());
+		
+		meld3.addTile(new Tile(1,6));
+		assertTrue(meld3.checkRun(meld3.getTiles()));
 	}
 }
