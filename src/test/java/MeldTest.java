e31f5f040dfe8da0d4c9d7c34a46d583cdfb0efa
korede97 <korede97@users.noreply.github.com>
2018-10-15 08:45:44 -0400

    failed testCheckRun

--- src/test/java/MeldTest.java
+++ src/test/java/MeldTest.java
@@ -6,7 +6,7 @@ public class MeldTest extends TestCase {
 		
 		PlayerHand hand = new PlayerHand ("x"); 
 		Tile t1 = new Tile(1,10); 
-		Tile t2 = new Tile(1, 11); 
+		Tile t2 = new Tile(1,11); 
 		Tile t3 = new Tile(1,12); 
 		Tile t4 = new Tile(1,13); 
 		
@@ -18,5 +18,24 @@ public class MeldTest extends TestCase {
 		Meld meld = new Meld (hand.getTiles());
 		
 		assertTrue(meld.getStatus());
+	}
+	
+	public void testCheckRun() {
+		
+		PlayerHand hand = new PlayerHand ("x"); 
+		Tile t1 = new Tile(1,10); 
+		Tile t2 = new Tile(1,11); 
+		Tile t3 = new Tile(1,12); 
+		Tile t4 = new Tile(1,13); 
+		
+		hand.addTileToHand(t1);
+		hand.addTileToHand(t2);
+		hand.addTileToHand(t3);
+		hand.addTileToHand(t4);
+
+		Meld meld = new Meld (hand.getTiles());
+		
+		AssertTrue(meld.checkRun()); 
+		
 	}
 }
