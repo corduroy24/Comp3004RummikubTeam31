fe5d6a29fbfc18ae441f0e68dbbd3bd5fb6bcfeb
korede97 <korede97@users.noreply.github.com>
2018-10-30 21:09:53 -0400

    Added Test for start of game.   - failed test for checking players turn

---
+++ src/test/java/TestPlan.java
@@ -0,0 +1,24 @@
+import junit.framework.TestCase;
+
+public class TestPlan extends TestCase{
+	
+	public void testGameStart() {
+		GameMaster game = new GameMaster(); 
+		
+		//Req. 1
+		game.dealInitialHand();
+		
+		assertEquals(14, game.getHuman().getHand().sizeOfHand()); 
+		assertEquals(14, game.getAI().getHand().sizeOfHand()); 
+		assertEquals(14, game.getAI2().getHand().sizeOfHand()); 
+		assertEquals(14, game.getAI3().getHand().sizeOfHand()); 
+		
+		//Req. 2
+		game.getHuman().play(); 
+		assertTrue(game.getHuman().getIsTurn()); 
+		assertFalse(game.getAI().getIsTurn()); 
+		assertFalse(game.getAI2().getIsTurn()); 
+		assertFalse(game.getAI3().getIsTurn()); 
+
+	}
+}
