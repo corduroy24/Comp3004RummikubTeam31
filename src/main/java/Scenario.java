9ac863e3586be073df80b1eefb8a281d05ed14a4
korede97 <korede97@users.noreply.github.com>
2018-11-29 19:44:59 -0500

    Changed Setup of scenario

--- src/main/java/Scenario.java
+++ src/main/java/Scenario.java
@@ -14,18 +14,24 @@ public interface Scenario {
 			game.addPlayer(3);
 			game.addPlayer(4);
 
-			
-			Tile t6[] = {new Tile(1,4), new Tile(1,5), new Tile(1,8)}; 
+			game.getAI().setIsfirstMeldComplete(true);
+			game.getAI2().setIsfirstMeldComplete(true);
+			game.getAI3().setIsfirstMeldComplete(true);
+			game.getAI4().setIsfirstMeldComplete(true);
+
+			Tile t6[] = {new Tile(1,4), new Tile(1,5), new Tile(1,6)}; 
 			ArrayList<Tile> listTiles = new ArrayList<Tile>(); 
 			for(int i = 0; i< t6.length; i++) listTiles.add(t6[i]); 
 			
 			game.getTable().addTiles(listTiles); 
+			
+			System.out.println("-------------" +game.getTable().getNumberOfTile());
 
-			game.getAI().getHand().DrawThis(new Tile(1,9), deck);
-			game.getAI().getHand().DrawThis(new Tile(4,5), deck);
-			game.getAI().getHand().DrawThis(new Tile(2,3), deck);
-			game.getAI().getHand().DrawThis(new Tile(2,9), deck);
-			game.getAI().getHand().DrawThis(new Tile(3,10), deck);
+			game.getAI().getHand().DrawThis(new Tile(1,1), deck);
+			game.getAI().getHand().DrawThis(new Tile(2,1), deck);
+			game.getAI().getHand().DrawThis(new Tile(3,1), deck);
+			game.getAI().getHand().DrawThis(new Tile(1,3), deck);
+			game.getAI().getHand().DrawThis(new Tile(1,7), deck);
 			game.getAI().getHand().DrawThis(new Tile(4,7), deck);
 
 			game.getAI2().getHand().DrawThis(new Tile(1,4), deck);
@@ -35,18 +41,19 @@ public interface Scenario {
 			game.getAI2().getHand().DrawThis(new Tile(2,5), deck);
 			//game.getAI2().getHand().DrawThis(new Tile(3,5), deck);
 			
-			game.getAI3().getHand().DrawThis(new Tile(1,4), deck);
-			game.getAI3().getHand().DrawThis(new Tile(1,5), deck);
-			game.getAI3().getHand().DrawThis(new Tile(1,6), deck);
-			game.getAI3().getHand().DrawThis(new Tile(1,5), deck);
-			game.getAI3().getHand().DrawThis(new Tile(2,5), deck);
+			game.getAI3().getHand().DrawThis(new Tile(2,7), deck);
+			game.getAI3().getHand().DrawThis(new Tile(2,8), deck);
+			game.getAI3().getHand().DrawThis(new Tile(2,9), deck);
+			game.getAI3().getHand().DrawThis(new Tile(2,10), deck);
+			game.getAI3().getHand().DrawThis(new Tile(3,5), deck);
 
 			game.getAI4().getHand().DrawThis(new Tile(3,4), deck);
 			game.getAI4().getHand().DrawThis(new Tile(4,5), deck);
 			game.getAI4().getHand().DrawThis(new Tile(3,6), deck);
 			game.getAI4().getHand().DrawThis(new Tile(4,8), deck);
 			game.getAI4().getHand().DrawThis(new Tile(2,9), deck);
-			//game.Announcement();
+			
+			game.Announcement();
 			return game; 
 		}
 		public GameMaster secondTurn (GameMaster game) {
